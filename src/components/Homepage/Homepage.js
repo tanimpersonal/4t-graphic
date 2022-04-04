7bc19cb77e8a20709b9a60dd196da53dfe656ef5
tanimpersonal <tashintanim2019@gmail.com>
2022-04-05 00:15:43 +0600

    homepage edit with detail

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { Carousel } from "react-responsive-carousel";
 import icon from "../../Images/my icon.png";
 import useData from "../../utility/useData";
@@ -6,6 +8,7 @@ import Review from "../Review/Review";
 import "./Homepage.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useNavigate } from "react-router-dom";
+AOS.init();
 const Homepage = () => {
   const [reviews, setReviews] = useData();
   let navigate = useNavigate();
@@ -14,12 +17,23 @@ const Homepage = () => {
   };
   return (
     <section>
-      <div className="flex items-center mx-10 justify-between">
-        <div className="text">
-          <h1 className="text-4xl mb-5 font-bold">4TGraphic</h1>
-          <h1 className="text-4xl font-bold">We Believe in Work</h1>
+      <div className="flex items-center mx-10 justify-between my-20">
+        <div className="text w-2/3 px-20">
+          <h1
+            data-aos="fade-up-right"
+            className="text-8xl mb-5 font-extrabold text-[#766DF4]"
+          >
+            4TGraphic
+          </h1>
+          <h1 className="text-4xl font-bold mb-5">We Believe in Work</h1>
+          <p>
+            We’re 4TGraphic. Bangladesh is where we are now based. Our
+            e-commerce website experience stretches back ten years. Our product
+            is solid, has a good reputation, and is trusted by people of all
+            sorts of startup ventures.
+          </p>
         </div>
-        <div className="image">
+        <div className="image" data-aos="zoom-in-up">
           <img src={icon} alt="" />
         </div>
       </div>
@@ -27,7 +41,7 @@ const Homepage = () => {
         Our Customer Says!
       </h1>
       {/* Carousel start */}
-      <div>
+      <div className="mx-10">
         <Carousel
           autoPlay={true}
           infiniteLoop={true}
