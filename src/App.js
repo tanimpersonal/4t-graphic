c3cf49fccb7aac16d44d836bbae562a661cd0919
tanimpersonal <tashintanim2019@gmail.com>
2022-04-04 08:45:58 +0600

    router initialized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Review from "./components/Review/Review";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="review" element={<Review />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,16 @@
 import React from "react";
+import CustomLink from "../CustomLink/CustomLink";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="nav">
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/dashboard">Dashboard</CustomLink>
+        <CustomLink to="/review">Review</CustomLink>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
