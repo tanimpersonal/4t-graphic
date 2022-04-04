2c530493ccfeb7d5bc501c4186b041c5920f7a76
tanimpersonal <tashintanim2019@gmail.com>
2022-04-04 08:09:57 +0600

    router and title added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Hola</h1>
+    </div>
+  );
 }
 
 export default App;
