f0c7a5e587bd30716af5ff11314d3cf93fb31db9
tanimpersonal <tashintanim2019@gmail.com>
2022-04-04 08:50:42 +0600

    tailwind added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import CustomLink from "../CustomLink/CustomLink";
-
+import "./Header.css";
 const Header = () => {
   return (
     <div>
-      <div className="nav">
+      <div className="nav flex text-2xl">
         <CustomLink to="/">Home</CustomLink>
         <CustomLink to="/dashboard">Dashboard</CustomLink>
         <CustomLink to="/review">Review</CustomLink>
