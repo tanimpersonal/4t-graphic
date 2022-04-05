653bc8850a7838c7449b33e426d0de40ba9e181e
tanimpersonal <tashintanim2019@gmail.com>
2022-04-05 10:14:13 +0600

    chart border added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,10 +5,10 @@ import CustomLineChart from "../CustomLineChart/CustomLineChart";
 const Dashboard = () => {
   return (
     <div className="mx-10 my-10">
-      <div className="bar-chart">
+      <div className="bar-chart border py-2">
         <CustomBarChart></CustomBarChart>
       </div>
-      <div className="line-chart mt-10">
+      <div className="line-chart mt-10 border py-2">
         <CustomLineChart></CustomLineChart>
       </div>
     </div>
